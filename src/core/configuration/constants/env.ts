79f6bab40f777ae55d42ca0f36414a576eb588e3
Alan Wendorff <alanwff4@gmail.com>
2024-10-08 23:57:27 -0300

    fixed bugs

--- src/core/configuration/constants/env.ts
+++ src/core/configuration/constants/env.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.API_URL ?? process.env.NEXT_PUBLIC_POKEAPI_URL;
+export const BASE_URL = process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL;
 //export const API_KEY = process.env.API_KEY; *If its necesary
